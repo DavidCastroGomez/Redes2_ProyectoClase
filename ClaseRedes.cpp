b7fa9f46ef15354c62e15eb54913d50e6e7821d9
DavidCastroGomez <99954770+DavidCastroGomez@users.noreply.github.com>
2024-02-28 10:51:15 +0100

    imported graphics library and created window

--- ClaseRedes.cpp
+++ ClaseRedes.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
 #include <conio.h>
 #include <SFML/Network.hpp>
+#include <SFML/Graphics.hpp>
 
 #include "ConsoleControl.h"
 #include "Chat.h"
 
 void RunClient();
 void RunServer();
+void RunWindows();
 
 unsigned short port = 3001;
 
@@ -15,6 +17,9 @@ int main()
     std::cout << "Select" << std::endl << "Client -> C" << std::endl << "Server -> S" << std::endl;
  
     char mode = ' ';
+
+
+    RunWindows();
 
     do {
 
@@ -33,6 +38,7 @@ int main()
             RunServer();
             break;
     }
+
 
     while (true) {
 
@@ -57,5 +63,33 @@ void RunServer() {
     std::cout << "Server";
 
     Chat* chat = Chat::Server(port);
+
+}
+
+void RunWindows() {
+
+    sf::RenderWindow windows;
+
+    windows.create(sf::VideoMode(800, 600), "Chat");
+    windows.setFramerateLimit(60);
+
+    sf::Text label;
+
+    sf::Font font;
+    font.loadFromFile("Minecraft.ttf");
+
+    label.setFont(font);
+    label.setCharacterSize(16);
+    label.setFillColor(sf::Color::White);
+    label.setString("Hello World");
+    label.setOrigin(0.5, 0.5);
+
+    label.setPosition(windows.getSize().x * 0.5, windows.getSize().y * 0.5);
+
+    while (windows.isOpen()) {
+        windows.clear(sf::Color::Black);
+        windows.draw(label);
+        windows.display();
+    }
 
 }
